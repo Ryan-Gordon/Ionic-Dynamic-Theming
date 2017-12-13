e499c3546aac66d4715b39b25eae36947b654c20
Ryan Gordon <ryangordon210@gmail.com>
2017-12-13 19:01:39 +0000

    Added Theme switcher in homepage + app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import {ThemeProvider} from '../providers/theme/theme'
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
-
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  selectedTheme: String;
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public theme: ThemeProvider) {
+    //Subscribe to the result of getActiveTheme; When the result changes, the new value will be passed to this subscriber.
+    this.theme.getActiveTheme().subscribe(val => this.selectedTheme = val);
+    
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {ThemeProvider} from '../../providers/theme/theme'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  selectedTheme: String;
+  constructor(public navCtrl: NavController,public theme:ThemeProvider) {
+    //Subscribe to the result of getActiveTheme; When the result changes, the new value will be passed to this subscriber.
 
-  constructor(public navCtrl: NavController) {
-
+    this.theme.getActiveTheme().subscribe(val => this.selectedTheme = val);
+    
   }
+
+
+  toggleAppTheme() {
+    if (this.selectedTheme === 'dark-theme') {
+      this.theme.setActiveTheme('light-theme');
+    } else {
+      this.theme.setActiveTheme('dark-theme');
+    }
+  }
+ 
 
 }
